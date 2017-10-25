ec1679eaa933d9be8d7f1b42abf874a13d917560
Aminur Rahman <rony.phdu@gmail.com>
2017-10-25 16:45:17 +0600

    Upto forms.

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Board
 
 # Create your views here.
@@ -13,3 +14,13 @@ def home(request):
         boards_names.append(board.name)
     response_html = '<br>'.join(boards_names)
     return HttpResponse(response_html)
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    # try:
+    #     board = Board.objects.get(pk=pk)
+    # except Board.DoesNotExist:
+    #     raise Http404
+    return render(request, 'topics.html', {'board': board})
+
+
