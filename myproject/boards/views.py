518261903d7d10cc86b7342f36f9cd4c590da56d
Aminur Rahman <rony.phdu@gmail.com>
2017-11-01 20:28:10 +0600

    Part two finished. this commot marked in docx file in part three.

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import NewTopicForm
 from django.http import HttpResponse
 from django.http import Http404
-from .models import Board
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -23,4 +26,24 @@ def board_topics(request, pk):
     #     raise Http404
     return render(request, 'topics.html', {'board': board})
 
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+                )
+            return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
 
