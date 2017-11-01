5e7088ae797b68184a6b41f2ba2605d6f8f2cf9d
Aminur Rahman <rony.phdu@gmail.com>
2017-11-02 02:47:37 +0600

    Part three finish in this commit.

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -5,7 +5,7 @@ from .views import home, board_topics, new_topic
 from .views import home, board_topics
 from .models import Board,Topic, Post
 from django.contrib.auth.models import User
-
+from .forms import NewTopicForm
 
 # Create your tests here.
 
@@ -127,3 +127,20 @@ class NewTopicTests(TestCase):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(new_topic_url)
         self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+
+    def test_contains_form(self):  # <- new test
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
+    def test_new_topic_invalid_post_data(self):  # <- updated this one
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
